cd12e68be9206e1294b8cbac93ee1666e99491b7
citrusian <115960900+citrusian@users.noreply.github.com>
2023-10-22 10:58:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobprogtk4/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.mobprogtk4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobprogtk4.ui.theme.MobProgTK4Theme
+
+// Preview, call MainContent function
+@Preview(showBackground = true)
+@Composable
+fun CalculatorPreview() {
+//    var calculatorVisibilityViewModel = viewModel<CalculatorVisibilityViewModel>()
+//    MainContent(calculatorVisibilityViewModel)sdfsdf
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobProgTK4Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobProgTK4Theme {
+        Greeting("Android")
+    }
+}
